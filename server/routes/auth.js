d4fb4d4ab035eb79393907bebae234cf4e86d0ca
h1dd3ngod <nikbog03@gmail.com>
2023-04-11 13:37:35 +0300

    feat: bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,29 +1,57 @@
 const router = require('express').Router()
-const DB = require("./config");
+const DB = require("../config");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const bcrypt = require('bcrypt')
 
-app.post('/register', async (req, res, next) => {
+const saltRounds = 10
+
+router.post('/register', async (req, res, next) => {
     const {username, password} = req.body
-    const exists = await DB.from('users').select('*').eq('username', username)
-    if (exists.data.length === 0) return res.sendStatus(500).json()
-    const hashedPassword = bcrypt.hash(password, 1, (err, hash) => {
-        if (!err) return hash
-        else console.error(err)
+    const exists = await (await DB.from('users').select('*').eq('username', username))
+    console.log(exists)
+    if (exists.error) {
+        console.error(exists.error)
+        return res.sendStatus(500);
+    }
+    // return res.status(400).json({ error: "User already exists" });
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+       if (err) {
+        console.error(err)
+        res.sendStatus(500)
+       }
+       const { written, error: insertError } = await DB.from('users').insert({ username, password: hash })
+       if (insertError) {
+        console.error(insertError)
+        return res.sendStatus(500)
+       }
+       console.log(written)
+       res.status(201).json(written)
     })
-    res.json(hashedPassword)
+    // res.json({body: hashedPassword})
 })
 
-app.post("/login", (req, res) => {
+router.post("/login", (req, res) => {
     // Verify user credentials and generate JWT token
     const {username, password} = req.body
     // const username = req.body.username;
     // const password = req.body.password;
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) return res.sendStatus(500)
+        bcrypt.compare(password, hash, function (err, result) {
+            if (err) {
+                console.error(err);
+            } else if (result) {
+                console.log("Passwords match!");
+            } else {
+                console.log("Passwords do not match!");
+            }
+        });
 
-    if (username !== "user" || password !== "password") {
-        return res.status(401).send("Invalid credentials").json();
-    }
+    })
+    // if (username !== "user" || password !== "password") {
+    //     return res.status(401).send("Invalid credentials").json();
+    // }
 
     const token = jwt.sign({ username }, secretKey);
 
